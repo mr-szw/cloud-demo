8b5ef079f1fa9dad4765a61541a88066600bbea6
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2020-01-15 08:31:04 +0800

    add zuul netty

---
+++ zuul-2-demo/src/main/java/com/sinbad/cloud/demo/SpringZuul2Application.java
@@ -0,0 +1,17 @@
+package com.sinbad.cloud.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+//开启zuul代理
+@EnableZuulProxy
+public class SpringZuul2Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZuul2Application.class, args);
+    }
+}

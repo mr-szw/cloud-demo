83ba3ebaa8d486c65bc97fac59fb25b45aa3597f
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2020-01-22 15:45:40 +0800

    test client

--- config-client/src/main/java/com/sinbad/cloud/demo/config/client/controller/GitConfigClientController.java
+++ config-client/src/main/java/com/sinbad/cloud/demo/config/client/controller/GitConfigClientController.java
@@ -1,20 +1,36 @@
 package com.sinbad.cloud.demo.config.client.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
-public class GitController {
+public class GitConfigClientController {
+	
+	
+	@Value("${data.env}")
+	private String dataEnv;
+	
+	
+	@Value("${data.datasource.datasource-demo.username}")
+	private String dataSourceDemoUserName;
+	
 	
 	@GetMapping(value = "show")
 	public Object show() {
+		
+		System.out.println("dataEnv " + dataEnv);
+		
 		return "gitConfig";
 	}
 	
 	@GetMapping(value = "autoShow")
 	public Object autoShow() {
-		return "gitAutoRefreshConfig";
+		
+		System.out.println("dataSourceDemoUserName " + dataSourceDemoUserName);
+		
+		return dataSourceDemoUserName;
 	}
 }
